e484f2fc23166cc35585c6a013eb8d02b8d52702
mitrape <miti1383@gmail.com>
2024-03-23 17:17:28 +0330

    west panel done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame {
+    private Font defaultFont = new Font("Bernard MT Condensed",Font.PLAIN,20);
     public Main (){
+        // initial designs
         setTitle("Amusement Park");
         setSize(800,800);
         setLocationRelativeTo(null);
@@ -18,37 +20,49 @@ public class Main extends JFrame {
         JPanel LeftPanel = new JPanel();
         LeftPanel.setLayout(new BoxLayout(LeftPanel, BoxLayout.Y_AXIS));
 
+
+        //adding a title for this part of amusement park
+        JTextField SlotMachine = new JTextField("Slot Machines");
+        SlotMachine.setPreferredSize(new Dimension(1,1));
+        SlotMachine.setFont(defaultFont);
+        LeftPanel.add(SlotMachine);
+
+        //adding Green slot machine
         Icon GreenMachine = new ImageIcon("D:/programming projects/Amusement Park/image/GreenMachine.PNG");
         JButton GreenSlotMachine = new JButton(GreenMachine);
         GreenSlotMachine.setBackground(Color.WHITE);
         LeftPanel.add(GreenSlotMachine);
 
 
+        //adding red slot machine
         Icon RedMachine = new ImageIcon("D:/programming projects/Amusement Park/image/RedMachine.PNG");
         JButton RedSlotMachine = new JButton(RedMachine);
         RedSlotMachine.setBackground(Color.WHITE);
         LeftPanel.add(RedSlotMachine);
 
 
+        //adding blue slot machine
         Icon BlueMachine = new ImageIcon("D:/programming projects/Amusement Park/image/BlueMachine.PNG");
         JButton BlueSlotMachine = new JButton(BlueMachine);
         BlueSlotMachine.setBackground(Color.WHITE);
         LeftPanel.add(BlueSlotMachine);
 
 
+        //adding black slot machine
         Icon BlackMachine = new ImageIcon("D:/programming projects/Amusement Park/image/BlackMachine.PNG");
         JButton BlackSlotMachine = new JButton(BlackMachine);
         BlackSlotMachine.setBackground(Color.WHITE);
         LeftPanel.add(BlackSlotMachine);
 
 
+        //adding white slot machine
         Icon WhiteMachine = new ImageIcon("D:/programming projects/Amusement Park/image/WhiteMachine.PNG");
         JButton WhiteSlotMachine = new JButton(WhiteMachine);
         WhiteSlotMachine.setBackground(Color.BLACK);
         LeftPanel.add(WhiteSlotMachine);
 
 
-        add (LeftPanel , BorderLayout.PAGE_START);
+        add (LeftPanel , BorderLayout.WEST);
         setVisible(true);
 
 
