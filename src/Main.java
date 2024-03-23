3dc1bd79f2a9a223cb0313d56859a8038e473981
mitrape <miti1383@gmail.com>
2024-03-23 20:10:29 +0330

    first step for creating shop

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class Main extends JFrame {
     private Font defaultFont = new Font("Bernard MT Condensed",Font.PLAIN,20);
+    GridBagConstraints gbc = new GridBagConstraints();
     public Main (){
         // initial designs
         setTitle("Amusement Park");
@@ -16,6 +17,7 @@ public class Main extends JFrame {
         initRightPanel();
         initBotPanel();
     }
+    // put all the slot machines
     private void initLeftPanel (){
         JPanel LeftPanel = new JPanel();
         LeftPanel.setLayout(new BoxLayout(LeftPanel, BoxLayout.Y_AXIS));
@@ -67,7 +69,96 @@ public class Main extends JFrame {
 
 
     }
+    // design store
     private void initRightPanel (){
+        JPanel RightPanel = new JPanel(new GridBagLayout());
+        gbc.insets = new Insets(2,2,2,2);
+
+
+        Icon PrizeClaw = new ImageIcon("D:/programming projects/Amusement Park/image/PrizeClawCard.PNG");
+        JButton PrizeClaw1 = new JButton(PrizeClaw);
+        PrizeClaw1.setBackground(Color.WHITE);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        RightPanel.add(PrizeClaw1,gbc);
+
+        JButton PrizeClaw2 = new JButton(PrizeClaw);
+        PrizeClaw2.setBackground(Color.WHITE);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        RightPanel.add(PrizeClaw2,gbc);
+
+        JButton PrizeClaw3 = new JButton(PrizeClaw);
+        PrizeClaw3.setBackground(Color.WHITE);
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        RightPanel.add(PrizeClaw3,gbc);
+
+        JButton FirstLevel1 = new JButton();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        RightPanel.add(FirstLevel1,gbc);
+
+        JButton FirstLevel2 = new JButton();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        RightPanel.add(FirstLevel2,gbc);
+
+        JButton FirstLevel3 = new JButton();
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        RightPanel.add(FirstLevel3,gbc);
+
+        JButton FirstLevel4 = new JButton();
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        RightPanel.add(FirstLevel4,gbc);
+
+        JButton SecondLevel1 = new JButton();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        RightPanel.add(SecondLevel1,gbc);
+
+        JButton SecondLevel2 = new JButton();
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        RightPanel.add(SecondLevel2,gbc);
+
+        JButton SecondLevel3 = new JButton();
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        RightPanel.add(SecondLevel3,gbc);
+
+        JButton SecondLevel4 = new JButton();
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        RightPanel.add(SecondLevel4,gbc);
+
+        JButton ThirdLevel1 = new JButton();
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        RightPanel.add(ThirdLevel1,gbc);
+
+        JButton ThirdLevel2 = new JButton();
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        RightPanel.add(ThirdLevel2,gbc);
+
+        JButton ThirdLevel3 = new JButton();
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        RightPanel.add(ThirdLevel3,gbc);
+
+        JButton ThirdLevel4 = new JButton();
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        RightPanel.add(ThirdLevel4,gbc);
+
+
+        add(RightPanel , BorderLayout.EAST);
+        setVisible(true);
+
+
 
     }
     private void initBotPanel (){
