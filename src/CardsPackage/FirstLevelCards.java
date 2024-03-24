4a9bae9b9229b11f69dfedfe423c7bf74795e258
mitrape <miti1383@gmail.com>
2024-03-24 20:55:24 +0330

    first edition of FirstLevelCards

---
+++ src/CardsPackage/FirstLevelCards.java
@@ -0,0 +1,11 @@
+package CardsPackage;
+public class FirstLevelCards {
+    public String [][]  ColorOfCoinFirst  = {{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"WHITE","RED"},{"GREEN","BLACK"},{"RED","BLUE"},
+            {"BLUE","GREEN"},{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"WHITE","RED"},{"GREEN","BLACK"},{"RED","BLUE"},{"BLUE","GREEN"},{"BLUE","WHITE"}};
+    public int [][] CountOfCoinFirst = {{2,2},{3,2},{2,3},{1,3},{3,1},{1,4},{4,1},{2,4},{4,2},{3,3},{2,2},{3,2},{2,3},{1,3},{3,1}};
+    public int [] ScoreFirst = {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+    public String [] CountOfSpecialCoinFirst = {"GREEN=1","BLUE=1","BLACK=1","WHITE=1","RED=1","GREEN=1","BLUE=1","BLACK=1","WHITE=1","RED=1","GREEN=1","BLUE=1",
+            "BLACK=1","WHITE=1","RED=1"};
+    public static int countFirst = 0;
+
+}
