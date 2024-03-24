dad6abe3d7ec3aed2edee6a3394c8c7091fd95bb
mitrape <miti1383@gmail.com>
2024-03-24 19:28:13 +0330

    first edition of ZeroLevelCards class

---
+++ src/CardsPackage/ZeroLevelCards.java
@@ -0,0 +1,8 @@
+package CardsPackage;
+public class ZeroLevelCards {
+    public String [] [] ColorOfCoin = {{"RED","BLUE","BLACK"},{"WHITE","GREEN","RED"}};
+    public int [] [] CountOfCoin = {{4,5,6},{4,5,6}};
+    public int [] score = {3,4,3};
+    public static int count = 0;
+
+}
