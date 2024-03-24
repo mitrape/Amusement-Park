2cc60778062907b607e3b0ff6551a562321575db
mitrape <miti1383@gmail.com>
2024-03-24 15:44:38 +0330

    first edit of player class

---
+++ src/PlayerPackage/Player.java
@@ -0,0 +1,17 @@
+package PlayerPackage ;
+public class Player {
+    public int RedCoin ;
+    public int GreenCoin ;
+    public int BlueCoin ;
+    public int WhiteCoin ;
+    public int BlackCoin ;
+    public int SpecialRedCoin ;
+    public int SpecialGreenCoin ;
+    public int SpecialBlueCoin ;
+    public int SpecialWhiteCoin ;
+    public int SpecialBlackCoin ;
+    public int GoldCoin ;
+    public int Score ;
+
+
+}
