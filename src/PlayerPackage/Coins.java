15a33295f9dec1b62281421a67e1775f5ec224b4
mitrape <miti1383@gmail.com>
2024-03-25 23:02:37 +0330

    first edition of Coins

---
+++ src/PlayerPackage/Coins.java
@@ -0,0 +1,9 @@
+package PlayerPackage ;
+public class Coins {
+        public static int GoldCoin = 5 ;
+        public static int RedCoin = 4 ;
+        public static int BlueCoin = 4;
+        public static int GreenCoin = 4 ;
+        public static int BlackCoin = 4 ;
+        public static int WhiteCoin = 4;
+}
