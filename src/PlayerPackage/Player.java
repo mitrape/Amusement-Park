a10e43bc1cbc8b045a7ad68cafab32e9c1b43e18
mitrape <miti1383@gmail.com>
2024-03-28 16:49:18 +0330

    add reserve cards attributes

--- src/PlayerPackage/Player.java
+++ src/PlayerPackage/Player.java
@@ -1,4 +1,7 @@
 package PlayerPackage ;
+
+import javax.swing.*;
+
 public class Player {
     public int RedCoin ;
     public int GreenCoin ;
@@ -12,6 +15,16 @@ public class Player {
     public int SpecialBlackCoin ;
     public int SpecialGoldCoin ;
     public int Score ;
+    public int ScoreReserve1 , ScoreReserve2 , ScoreReserve3;
+    public String SpecialCoinReserve1 , SpecialCoinReserve2 , SpecialCoinReserve3 ;
+    public int [] CoinReserve1 = new int [5] ;
+    public int [] CoinReserve2 = new int [5] ;
+    public int [] CoinReserve3 = new int [5] ;
+    public int CountReserveCard ;
+
+    public JButton ReserveCard1 = new JButton("Card1");
+    public JButton ReserveCard2 = new JButton("Card2");
+    public JButton ReserveCard3 = new JButton("Card3");
 
 
 
