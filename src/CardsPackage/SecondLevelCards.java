8082196ad67528062f98b5f51629157e995f6066
mitrape <miti1383@gmail.com>
2024-03-31 02:26:07 +0330

    final SecondLevelCards

--- src/CardsPackage/SecondLevelCards.java
+++ src/CardsPackage/SecondLevelCards.java
@@ -4,7 +4,7 @@ public class SecondLevelCards {
     public final String [][]  ColorOfCoinSecond  = {{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"WHITE","RED"},
             {"WHITE","RED"},{"GREEN","BLACK"},{"RED","BLUE"},{"BLUE","GREEN"},{"GREEN","BLACK"},{"RED","BLUE"},{"BLUE","GREEN"},{"BLUE","WHITE"}};
 
-    public final int [][] CountOfCoinSecond = {{2,4},{4,2},{3,3},{3,4},{4,3},{5,2},{2,5},{4,4},{5,3},{3,5},{2,6},{6,2},{2,4},{2,4},{3,4}};
+    public final int [][] CountOfCoinSecond = {{2,4},{4,2},{3,3},{3,4},{4,3},{4,4},{2,4},{2,4},{3,4},{4,2},{3,3},{3,4},{4,3},{4,4},{2,4}};
 
     public final int [] Score = {2,3,4,2,4,2,3,2,4,3,3,2,4,2,3};
     public int  [] ColorAndCountSecond = new int [5]; // red = 0 , blue = 1 , white = 2 , green = 3 , black = 4
