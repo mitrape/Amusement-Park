6a9d700d0b855d523c10cf30f90078b1044ec731
mitrape <miti1383@gmail.com>
2024-03-30 14:52:08 +0330

    change require coins for this level cards to 3 type of coins

--- src/CardsPackage/ThirdLevelCards.java
+++ src/CardsPackage/ThirdLevelCards.java
@@ -1,9 +1,11 @@
 package CardsPackage ;
 public class ThirdLevelCards{
-    public final String [][]  ColorOfCoinThird  = {{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"RED","BLUE"},{"BLACK","WHITE"},{"GREEN","BLUE"},{"WHITE","RED"},
-            {"WHITE","RED"},{"GREEN","BLACK"},{"RED","BLUE"},{"BLUE","GREEN"},{"GREEN","BLACK"},{"RED","BLUE"},{"BLUE","GREEN"},{"BLUE","WHITE"}};
+    public final String [][]  ColorOfCoinThird  = {{"RED","BLUE","BLACK"},{"BLACK","WHITE","GREEN"},{"GREEN","BLUE","WHITE"},
+            {"RED","BLUE","GREEN"},{"RED","BLUE","BLACK"},{"BLACK","WHITE","GREEN"},{"GREEN","BLUE","WHITE"},{"RED","BLUE","GREEN"},
+            {"RED","BLUE","GREEN"},{"RED","BLUE","BLACK"},{"BLACK","WHITE","GREEN"},{"GREEN","BLUE","WHITE"},{"RED","BLUE","GREEN"},
+            {"RED","BLUE","BLACK"},{"BLACK","WHITE","GREEN"}};
 
-    public final int [][] CountOfCoinThird = {{3,4},{4,3},{5,2},{2,5},{4,4},{5,5},{5,3},{3,5},{2,6},{6,2},{5,4},{5,4},{3,6},{6,3},{4,6}};
+    public final int [][] CountOfCoinThird = {{1,2,4},{2,1,4},{2,2,3},{2,3,3},{3,3,3},{2,4,3},{4,3,3},{1,2,4},{2,1,4},{2,2,3},{2,3,3},{3,3,3},{2,4,3},{4,3,3},{3,3,3}};
 
     public final int [] Score = {3,4,5,5,3,4,5,3,4,3,4,5,4,5,3};
     public int  [] ColorAndCountThird = new int [5]; // red = 0 , blue = 1 , white = 2 , green = 3 , black = 4
@@ -16,55 +18,75 @@ public class ThirdLevelCards{
     public ThirdLevelCards (){
         if ( ColorOfCoinThird [CountThird][0] == "RED") {
             ColorAndCountThird[0] = CountOfCoinThird[CountThird][0];
-            CardThird ="RED="+ColorAndCountThird[0]+",";
+            CardThird =ColorAndCountThird[0]+"RED"+",";
         }
         else if ( ColorOfCoinThird [CountThird][0] == "BLUE") {
             ColorAndCountThird[1] = CountOfCoinThird[CountThird][0];
-            CardThird ="BLUE="+ColorAndCountThird[1]+",";
+            CardThird =ColorAndCountThird[1]+"BLUE"+",";
         }
         else if ( ColorOfCoinThird [CountThird][0] == "WHITE") {
             ColorAndCountThird[2] = CountOfCoinThird[CountThird][0];
-            CardThird ="WHITE="+ColorAndCountThird[2]+",";
+            CardThird =ColorAndCountThird[2]+"WHITE"+",";
         }
         else if ( ColorOfCoinThird [CountThird][0] == "GREEN") {
             ColorAndCountThird[3] = CountOfCoinThird[CountThird][0];
-            CardThird ="GREEN="+ColorAndCountThird[3]+",";
+            CardThird =ColorAndCountThird[3]+"GREEN"+",";
         }
         else {
             ColorAndCountThird[4] = CountOfCoinThird[CountThird][0];
-            CardThird ="BLACK="+ColorAndCountThird[4]+",";
+            CardThird =ColorAndCountThird[4]+"BLACK"+",";
         }
 
         if ( ColorOfCoinThird [CountThird][1] == "RED") {
             ColorAndCountThird[0] = CountOfCoinThird[CountThird][1];
-            CardThird=CardThird+"RED="+ColorAndCountThird[0];
+            CardThird=CardThird+ColorAndCountThird[0]+"RED"+",";
         }
         else if ( ColorOfCoinThird [CountThird][1] == "BLUE") {
             ColorAndCountThird[1] = CountOfCoinThird[CountThird][1];
-            CardThird=CardThird+"BLUE="+ColorAndCountThird[1];
+            CardThird=CardThird+ColorAndCountThird[1]+"BLUE"+",";
         }
         else if ( ColorOfCoinThird [CountThird][1] == "WHITE") {
             ColorAndCountThird[2] = CountOfCoinThird[CountThird][1];
-            CardThird=CardThird+"WHITE="+ColorAndCountThird[2];
+            CardThird=CardThird+ColorAndCountThird[2]+"WHITE"+",";
         }
         else if ( ColorOfCoinThird [CountThird][1] == "GREEN") {
             ColorAndCountThird[3] = CountOfCoinThird[CountThird][1];
-            CardThird=CardThird+"GREEN="+ColorAndCountThird[3];
+            CardThird=CardThird+ColorAndCountThird[3]+"GREEN"+",";
         }
         else {
             ColorAndCountThird[4] = CountOfCoinThird[CountThird][1];
-            CardThird=CardThird+"BLACK="+ColorAndCountThird[4];
+            CardThird=CardThird+ColorAndCountThird[4]+"BLACK"+",";
         }
 
+        if ( ColorOfCoinThird [CountThird][2] == "RED") {
+            ColorAndCountThird[0] = CountOfCoinThird[CountThird][2];
+            CardThird=CardThird+ColorAndCountThird[0]+"RED";
+        }
+        else if ( ColorOfCoinThird [CountThird][2] == "BLUE") {
+            ColorAndCountThird[1] = CountOfCoinThird[CountThird][2];
+            CardThird=CardThird+ColorAndCountThird[1]+"BLUE";
+        }
+        else if ( ColorOfCoinThird [CountThird][2] == "WHITE") {
+            ColorAndCountThird[2] = CountOfCoinThird[CountThird][2];
+            CardThird=CardThird+ColorAndCountThird[2]+"WHITE";
+        }
+        else if ( ColorOfCoinThird [CountThird][2] == "GREEN") {
+            ColorAndCountThird[3] = CountOfCoinThird[CountThird][2];
+            CardThird=CardThird+ColorAndCountThird[3]+"GREEN";
+        }
+        else {
+            ColorAndCountThird[4] = CountOfCoinThird[CountThird][2];
+            CardThird=CardThird+ColorAndCountThird[4]+"BLACK";
+        }
 
 
         this.ScoreThird = this.Score [CountThird];
 
         if (CountThird % 2 == 0){
-            this.ColorSpecialCoinThird = "Red";
+            this.ColorSpecialCoinThird = "White";
         }
         else {
-            this.ColorSpecialCoinThird = "Black";
+            this.ColorSpecialCoinThird = "Green";
         }
 
         CountThird ++ ;
